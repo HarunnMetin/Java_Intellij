b7ebd327988cd63b6b16332cfdf5c0402c9b1d4e
HarunnMetin <mtnmusa07@gmail.com>
2023-12-10 21:33:37 +0300

    v1

--- src/Day27_ParametreliConstructor_CobstructorCall/C01_Hastane.java
+++ src/Day27_ParametreliConstructor_CobstructorCall/C01_Hastane.java
@@ -2,7 +2,9 @@ package Day27_ParametreliConstructor_CobstructorCall;
 
 public class C01_Hastane {
 
-    public static void main(String[] args) {
+    static String hastaneAdi = "Yildiz Hastanesi";
+    static String hastaneTelefonu = "9030403409";
+    String personelIsmi = "isim atanmadi";
+    String personelTelefonu = "telefon atanmadi";
 
-    }
 }
---
+++ src/Day27_ParametreliConstructor_CobstructorCall/C02_HastaneRunner.java
@@ -0,0 +1,10 @@
+package Day27_ParametreliConstructor_CobstructorCall;
+
+public class C02_HastaneRunner {
+
+    public static void main(String[] args) {
+
+        C01_Hastane obj = new C01_Hastane();
+        System.out.println(obj.personelIsmi);
+    }
+}
