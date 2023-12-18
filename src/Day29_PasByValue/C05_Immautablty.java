81b9e7c385fb51f0811031a616c382fb102e8030
HarunnMetin <mtnmusa07@gmail.com>
2023-12-18 20:55:56 +0300

    java

---
+++ src/Day29_PasByValue/C05_Immautablty.java
@@ -0,0 +1,11 @@
+package Day29_PasByValue;
+
+public class C05_Immautablty {
+
+    public static void main(String[] args) {
+
+        String str = "Java Candir";
+
+        System.out.println();
+    }
+}
