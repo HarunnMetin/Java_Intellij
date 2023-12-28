e9fe2722dcc6e4fb75dfb8909b2a21ae0081e629
HarunnMetin <mtnmusa07@gmail.com>
2023-12-28 17:58:56 +0300

    java

---
+++ src/Day36_InheritanceDataTypeKullanimi/JParentClass.java
@@ -0,0 +1,13 @@
+package Day36_InheritanceDataTypeKullanimi;
+
+public class JParentClass {
+
+    public void method1(){
+
+        System.out.println("parent class method1");
+    }
+    public void method2(){
+
+        System.out.println("parent class metho2");
+    }
+}
---
+++ src/Day36_InheritanceDataTypeKullanimi/KChildClass.java
@@ -0,0 +1,14 @@
+package Day36_InheritanceDataTypeKullanimi;
+
+public class KChildClass extends JParentClass{
+
+    public void method1(){
+
+        System.out.println("child class method1");
+    }
+
+    @Override
+    public void method2() {
+        super.method2();
+    }
+}
