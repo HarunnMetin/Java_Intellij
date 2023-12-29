7ffd8af37a37c2526ba00c44cdec74a69d101114
HarunnMetin <mtnmusa07@gmail.com>
2023-12-29 23:07:07 +0300

    java

---
+++ src/Day38_AbstractClasses/A_KuralciParent.java
@@ -0,0 +1,15 @@
+package Day38_AbstractClasses;
+
+public abstract class A_KuralciParent {
+
+    public void method1(){
+        System.out.println("parent method 1");
+    }
+    public void method2(){
+        System.out.println("parent method 2");
+    }
+    public abstract void method3();
+    public abstract void method4();
+
+
+}
