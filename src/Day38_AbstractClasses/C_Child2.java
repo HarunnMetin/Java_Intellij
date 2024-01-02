4d149cd42b91c56483755a8e2459978645dd25b2
HarunnMetin <mtnmusa07@gmail.com>
2024-01-02 19:35:08 +0300

    java

---
+++ src/Day38_AbstractClasses/C_Child2.java
@@ -0,0 +1,15 @@
+package Day38_AbstractClasses;
+
+public class C_Child2 extends A_KuralciParent{
+
+
+    @Override
+    public void method3() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+}
