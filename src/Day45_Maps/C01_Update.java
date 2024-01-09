afe098386891916a6f8188ca13dd5abf071968e6
HarunnMetin <mtnmusa07@gmail.com>
2024-01-09 17:17:07 +0300

    java

---
+++ src/Day45_Maps/C01_Update.java
@@ -0,0 +1,13 @@
+package Day45_Maps;
+
+import Day44_Maps.MethodDeposu;
+
+import java.util.Map;
+
+public class C01_Update {
+
+    public static void main(String[] args) {
+
+        Map<Integer,String> ogrenciMap = MethodDeposu.ogrenciMapOlustur();
+    }
+}
---
+++ src/Day45_Maps/C02_SiniflariBirArttirma.java
@@ -0,0 +1,10 @@
+package Day45_Maps;
+
+public class C02_SiniflariBirArttirma {
+
+    public static void main(String[] args) {
+
+        //ogrenci mapinde yil sonu duzenlemesi yapalim 9,10,11. siniftaki ogrencilerin siniflarini
+        //bir arttiralim
+    }
+}
